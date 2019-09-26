85076a7057e6beedd08fee805fd828b5f3e9d0a5
thanhle <mix2pigcat@gmaail.com>
2019-09-26 17:25:15 +0700

    file bai4

---
+++ javascript.js
@@ -0,0 +1,6 @@
+function perimeter() {
+let radius,perimeter;
+radius=parseInt(document.getElementById('a').value);
+    perimeter=radius*2*3.14;
+    document.getElementById('kq').value= perimeter;
+}
